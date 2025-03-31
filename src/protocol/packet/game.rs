a2830c514509fa2f7b2c11f3f4c04b1a0f625743
The24Kings <24kingsunite@gmail.com>
2025-03-31 15:24:34 -0700

    added serialization for GAME packet

--- src/protocol/packet/game.rs
+++ src/protocol/packet/game.rs
@@ -15,7 +15,26 @@ pub struct Game {
 }
 
 impl<'a> Parser<'a> for Game {
-    fn serialize<W: Write>(&self, _writer: &mut W) -> Result<(), std::io::Error> {
+    fn serialize<W: Write>(&self, writer: &mut W) -> Result<(), std::io::Error> {
+        // Package into a byte array
+        let mut packet: Vec<u8> = Vec::new();
+
+        packet.push(self.message_type);
+        packet.extend(self.initial_points.to_le_bytes());
+        packet.extend(self.stat_limit.to_le_bytes());
+        packet.extend(self.description_len.to_le_bytes());
+        packet.extend(self.description.as_bytes());
+
+        // Write the packet to the buffer
+        writer.write_all(&packet).map_err(|_| {
+            std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "Failed to write packet to buffer",
+            )
+        })?;
+
+        println!("[GAME] Serialized packet: {:?}", packet);
+
         Ok(())
     }
 
@@ -36,14 +55,15 @@ impl<'a> Parser<'a> for Game {
                         std::io::ErrorKind::InvalidData,
                         format!("Failed to parse description: {}", e),
                     )
-                })?
+                })?,
         })
     }
 }
 
 impl std::fmt::Display for Game {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f,
+        write!(
+            f,
             "Game {{ initial_points: {}, stat_limit: {}, description_len: {}, description: {} }}",
             self.initial_points, self.stat_limit, self.description_len, self.description
         )
