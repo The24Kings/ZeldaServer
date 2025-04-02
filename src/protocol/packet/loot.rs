92ee8d45fc96bf943ec4e47718a1770227b835d3
The24Kings <24kingsunite@gmail.com>
2025-04-02 15:55:59 -0700

    added serializer and deserializer for LOOT packet

--- src/protocol/packet/loot.rs
+++ src/protocol/packet/loot.rs
@@ -12,13 +12,44 @@ pub struct Loot {
 }
 
 impl<'a> Parser<'a> for Loot {
-    fn serialize<W: Write>(&self, _writer: &mut W) -> Result<(), std::io::Error> {
-        // Implement serialization logic here
+    fn serialize<W: Write>(&self, writer: &mut W) -> Result<(), std::io::Error> {
+        // Package into a byte array
+        let mut packet: Vec<u8> = Vec::new();
+
+        packet.push(self.message_type);
+
+        let mut target_name_bytes = self.target_name.as_bytes().to_vec();
+        target_name_bytes.resize(32, 0x00); // Pad the name to 32 bytes
+        packet.extend(target_name_bytes);
+
+        // Send the packet to the author
+        writer.write_all(&packet).map_err(|_| {
+            std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "Failed to write packet to buffer",
+            )
+        })?;
+
+        println!("[LOOT] Serialized packet: {}",
+            packet
+                .iter()
+                .map(|b| format!("0x{:02x}", b))
+                .collect::<Vec<String>>()
+                .join(" ")
+        );
+
         Ok(())
     }
 
-    fn deserialize(_packet: Packet) -> Result<Self, std::io::Error> {
-        // Implement deserialization logic here
-        Ok(Self::default())
+    fn deserialize(packet: Packet) -> Result<Self, std::io::Error> {
+        let author = packet.author.clone();
+        let message_type = packet.message_type;
+        let target_name = String::from_utf8_lossy(&packet.body[0..32]).trim_end_matches('\0').to_string();
+        
+        Ok(Loot {
+            author,
+            message_type,
+            target_name,
+        })
     }
 }
