537a0b3e256844571d36305c4c56cc6af5ccc336
The24Kings <24kingsunite@gmail.com>
2025-04-02 15:59:13 -0700

    added serializer and deserializer for START packet

--- src/protocol/packet/start.rs
+++ src/protocol/packet/start.rs
@@ -20,12 +20,33 @@ impl Default for Start {
 }
 
 impl<'a> Parser<'a> for Start {
-    fn serialize<W: Write>(&self, _writer: &mut W) -> Result<(), std::io::Error> {
-        // Implement serialization logic here
+    fn serialize<W: Write>(&self, writer: &mut W) -> Result<(), std::io::Error> {
+        // Package into a byte array
+        let mut packet: Vec<u8> = Vec::new();
+
+        packet.push(self.message_type);
+        
+        // Send the packet to the author
+        writer.write_all(&packet).map_err(|_| {
+            std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "Failed to write packet to buffer",
+            )
+        })?;
+
+        println!("[START] Serialized packet: {}",
+            packet
+                .iter()
+                .map(|b| format!("0x{:02x}", b))
+                .collect::<Vec<String>>()
+                .join(" ")
+        );
         Ok(())
     }
-    fn deserialize(_packet: Packet) -> Result<Self, std::io::Error> {
-        // Implement deserialization logic here
-        Ok(Start::default())
+    fn deserialize(packet: Packet) -> Result<Self, std::io::Error> {
+        Ok(Start {
+            author: packet.author,
+            message_type: packet.message_type,
+        })
     }
 }
