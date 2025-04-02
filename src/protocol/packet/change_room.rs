3ad4a2396b898ee085de3bb4e588da3187ef7331
The24Kings <24kingsunite@gmail.com>
2025-04-02 15:35:43 -0700

    added serializer and deserializer for CHANGEROOM packet

--- src/protocol/packet/change_room.rs
+++ src/protocol/packet/change_room.rs
@@ -13,12 +13,41 @@ pub struct ChangeRoom {
 
 impl<'a> Parser<'a> for ChangeRoom {
     fn serialize<W: Write>(&self, _writer: &mut W) -> Result<(), std::io::Error> {
-        // Implement serialization logic here
+        // Package into a byte array
+        let mut packet: Vec<u8> = Vec::new();
+
+        packet.push(self.message_type);
+        packet.extend(self.room_num.to_le_bytes());
+
+        // Write the packet to the buffer
+        _writer.write_all(&packet).map_err(|_| {
+            std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "Failed to write packet to buffer",
+            )
+        })?;
+
+        println!("[CHANGE_ROOM] Serialized packet: {}",
+            packet
+                .iter()
+                .map(|b| format!("0x{:02x}", b))
+                .collect::<Vec<String>>()
+                .join(" ")
+        );
+        
         Ok(())
     }
 
-    fn deserialize(_packet: Packet) -> Result<Self, std::io::Error> {
+    fn deserialize(packet: Packet) -> Result<Self, std::io::Error> {
+        println!("[CHANGE_ROOM] Deserializing packet: {}", packet);
+
+        let room_num = u16::from_le_bytes([packet.body[0], packet.body[1]]);
+
         // Implement deserialization logic here
-        Ok(Self::default())
+        Ok(ChangeRoom { 
+            author: packet.author, 
+            message_type: packet.message_type,
+            room_num
+        })
     }
 }
