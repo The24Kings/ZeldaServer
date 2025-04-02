baac99e28f9d85bf83b4da2795b7896c93cb3fb0
The24Kings <24kingsunite@gmail.com>
2025-04-02 15:52:30 -0700

    added serializer and deserializer for LEAVE packet

--- src/protocol/packet/leave.rs
+++ src/protocol/packet/leave.rs
@@ -20,13 +20,36 @@ impl Default for Leave {
 }
 
 impl<'a> Parser<'a> for Leave {
-    fn serialize<W: Write>(&self, _writer: &mut W) -> Result<(), std::io::Error> {
-        // Implement serialization logic here
+    fn serialize<W: Write>(&self, writer: &mut W) -> Result<(), std::io::Error> {
+        // Package into a byte array
+        let mut packet: Vec<u8> = Vec::new();
+
+        packet.push(self.message_type);
+
+        // Write the packet to the buffer
+        writer.write_all(&packet).map_err(|_| {
+            std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "Failed to write packet to buffer",
+            )
+        })?;
+
+        println!("[LEAVE] Serialized packet: {}",
+            packet
+                .iter()
+                .map(|b| format!("0x{:02x}", b))
+                .collect::<Vec<String>>()
+                .join(" ")
+        );
+        
         Ok(())
     }
 
-    fn deserialize(_packet: Packet) -> Result<Self, std::io::Error> {
+    fn deserialize(packet: Packet) -> Result<Self, std::io::Error> {
         // Implement deserialization logic here
-        Ok(Self::default())
+        Ok(Leave {
+            author: packet.author,
+            message_type: packet.message_type,
+        })
     }
 }
