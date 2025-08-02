a27d774a9d870db68af7e617de57b7d9b7d9d685
The24Kings <24kingsunite@gmail.com>
2025-08-02 12:39:59 -0700

    removed comments

--- src/protocol/error.rs
+++ src/protocol/error.rs
@@ -1,15 +1,16 @@
 #[derive(Default, Debug, Clone)]
+#[repr(u8)]
 pub enum ErrorCode {
     #[default]
-    Other,          // 0
-    BadRoom,        // 1
-    PlayerExists,   // 2
-    BadMonster,     // 3
-    StatError,      // 4
-    NotReady,       // 5
-    NoTarget,       // 6
-    NoFight,        // 7
-    NoPlayerCombat, // 8
+    Other,
+    BadRoom,
+    PlayerExists,
+    BadMonster,
+    StatError,
+    NotReady,
+    NoTarget,
+    NoFight,
+    NoPlayerCombat,
 }
 
 impl Into<u8> for ErrorCode {
