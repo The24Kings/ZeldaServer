f3576708fbf58d70c19453e34862d89722ac5629
The24Kings <24kingsunite@gmail.com>
2025-08-18 08:55:03 -0700

    take input

--- src/commands.rs
+++ src/commands.rs
@@ -1,7 +1,18 @@
+use std::io;
 use std::sync::mpsc::Sender;
 
 use crate::protocol::Protocol;
 
 pub fn input(_sender: Sender<Protocol>) -> ! {
-    loop {} // Take input from the console.
+    loop {
+        let mut input = String::new();
+
+        match io::stdin().read_line(&mut input) {
+            Ok(n) => {
+                println!("{n} bytes read");
+                println!("{input}");
+            }
+            Err(error) => println!("error: {error}"),
+        }
+    } // Take input from the console.
 }
