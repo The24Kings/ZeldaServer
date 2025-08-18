1ebf0bc7e79a5cd32c4524493f0ee2b8c0c61858
The24Kings <24kingsunite@gmail.com>
2025-08-18 09:57:07 -0700

    command parsing

--- src/commands.rs
+++ src/commands.rs
@@ -2,11 +2,21 @@ use std::io;
 use std::sync::mpsc::Sender;
 
 use serde::Serialize;
+use tracing::{error, info};
 
 use crate::protocol::Protocol;
 
 #[derive(Serialize)]
-pub struct Action;
+pub enum ActionKind {
+    MESSAGE,
+    OTHER,
+}
+
+#[derive(Serialize)]
+pub struct Action {
+    pub kind: ActionKind,
+    pub args: Vec<String>,
+}
 
 impl std::fmt::Display for Action {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -19,17 +29,38 @@ impl std::fmt::Display for Action {
     }
 }
 
-pub fn input(_sender: Sender<Protocol>) -> ! {
+pub fn input(sender: Sender<Protocol>) -> ! {
     loop {
         // Take input from the console.
         let mut input = String::new();
 
         match io::stdin().read_line(&mut input) {
-            Ok(n) => {
-                println!("{n} bytes read");
-                println!("{input}");
+            Ok(_) => {}
+            Err(e) => {
+                error!("Could not read stdin: {e}");
+                continue;
             }
-            Err(error) => println!("error: {error}"),
         }
+
+        if !input.starts_with("!") {
+            continue;
+        }
+
+        info!("[COMMAND] Parsing command.");
+
+        // Sanitize and Tokenize
+        let input = input[1..].trim().to_string().to_ascii_lowercase();
+        let argv: Vec<String> = input.split(' ').map(|s| s.to_string()).collect();
+
+        let kind = match argv[0].as_str() {
+            "message" => ActionKind::MESSAGE,
+            _ => ActionKind::OTHER,
+        };
+
+        sender
+            .send(Protocol::Command(Action { kind, args: argv }))
+            .unwrap_or_else(|_| {
+                error!("[COMMAND] Failed to send command packet");
+            })
     }
 }
